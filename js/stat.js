2e8a0ca235c8a706fa019fe0d7147e15d667b662
mika193 <36661070+mika193@users.noreply.github.com>
2018-06-02 21:21:22 +0300

    Правки в соответствии с замечаниями

--- js/stat.js
+++ js/stat.js
@@ -1,78 +1,116 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_CURVE = 20;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
 var GAP = 10;
-var LINE_HEIGHT = 30;
 var HISTOGRAM_HEIGHT = 150;
-var BAR_WIDHT = 40;
-var BAR_SPACE_BETWEEN = 50;
-var contentCordX = CLOUD_X + CLOUD_CURVE + GAP;
-var contentCordY = CLOUD_Y + CLOUD_CURVE + GAP;
-var barMaxHeight = HISTOGRAM_HEIGHT - LINE_HEIGHT;
+var TEXT_WIDTH = 200;
 
-var cloud = function (ctx, x, y, color) {
+var cloudParams = {
+  WIDTH: 420,
+  HEIGHT: 270,
+  CURVE: 20,
+  X: 100,
+  Y: 10
+};
+
+var barParams = {
+  WIDTH: 40,
+  GAP: 50
+};
+
+var fontParams = {
+  size: 16,
+  family: 'PT Mono',
+  baseline: 'hanging',
+};
+
+var lineHeight = fontParams.size * 1.4;
+var contentCordX = cloudParams.X + cloudParams.CURVE + GAP;
+var contentCordY = cloudParams.Y + cloudParams.CURVE + GAP;
+var barMaxHeight = HISTOGRAM_HEIGHT - lineHeight;
+var shadowColor = 'rgba(0, 0, 0, 0.7)';
+var cloudColor = 'rgba(255, 255, 255, 1)';
+var textColor = 'rgba(0, 0, 0, 1)';
+var barPlayerColor = 'rgba(255, 0, 0, 1)';
+
+var buildCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.moveTo(x, y);
-  ctx.lineTo(x + CLOUD_WIDTH / 2, y + CLOUD_CURVE);
-  ctx.lineTo(x + CLOUD_WIDTH, y);
-  ctx.lineTo(x + CLOUD_WIDTH - CLOUD_CURVE, y + CLOUD_HEIGHT / 2);
-  ctx.lineTo(x + CLOUD_WIDTH, y + CLOUD_HEIGHT);
-  ctx.lineTo(x + CLOUD_WIDTH / 2, y + CLOUD_HEIGHT - CLOUD_CURVE);
-  ctx.lineTo(x, y + CLOUD_HEIGHT);
-  ctx.lineTo(x + CLOUD_CURVE, y + CLOUD_HEIGHT / 2);
+  ctx.lineTo(x + cloudParams.WIDTH / 2, y + cloudParams.CURVE);
+  ctx.lineTo(x + cloudParams.WIDTH, y);
+  ctx.lineTo(x + cloudParams.WIDTH - cloudParams.CURVE, y + cloudParams.HEIGHT / 2);
+  ctx.lineTo(x + cloudParams.WIDTH, y + cloudParams.HEIGHT);
+  ctx.lineTo(x + cloudParams.WIDTH / 2, y + cloudParams.HEIGHT - cloudParams.CURVE);
+  ctx.lineTo(x, y + cloudParams.HEIGHT);
+  ctx.lineTo(x + cloudParams.CURVE, y + cloudParams.HEIGHT / 2);
   ctx.closePath();
   ctx.stroke();
   ctx.fill();
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
+var getMaxElement = function (array) {
+  return Math.max.apply(null, array);
+};
 
-  for (var i = 1; i < arr.length; i++) {
-    if (arr[i] > maxElement) {
-      maxElement = arr[i];
+var generateColor = function () {
+  var color = Math.ceil(Math.random() * 100) / 100;
+  if (color < 0.1) {
+    color = color += 0.08;
+  }
+
+  return 'rgba(0, 0, 255, ' + color + ')';
+};
+
+var writeText = function (ctx, text, maxWidth, textCordX, textCordY) {
+  var words = text.split(' ');
+  var line = '';
+
+  ctx.font = fontParams.size + 'px ' + fontParams.family;
+  ctx.textBaseline = fontParams.baseline;
+  ctx.fillStyle = textColor;
+
+  for (var i = 0; i < words.length; i++) {
+    var testLine = line + words[i] + ' ';
+    var testWidth = ctx.measureText(testLine).width;
+
+    if (testWidth > maxWidth) {
+      ctx.fillText(line, textCordX, textCordY);
+      line = words[i] + ' ';
+      textCordY += lineHeight;
+    } else {
+      line = testLine;
     }
   }
 
-  return maxElement;
+  ctx.fillText(line, textCordX, textCordY);
 };
 
-var buildingHistogram = function (ctx, x, y, width, height, color, name, nameY) {
+var buildHistogram = function (ctx, x, y, width, height, color, name, nameY) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
-  ctx.fillText(name, x, nameY);
+  writeText(ctx, name, width + barParams.GAP / 2, x, nameY);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  cloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-  cloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+  buildCloud(ctx, cloudParams.X + GAP, cloudParams.Y + GAP, shadowColor);
+  buildCloud(ctx, cloudParams.X, cloudParams.Y, cloudColor);
 
-  ctx.font = '16px PT Mono';
-  ctx.textBaseline = 'hanging';
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', contentCordX, contentCordY);
-  ctx.fillText('Список результатов:', contentCordX, contentCordY + LINE_HEIGHT);
+  writeText(ctx, 'Ура вы победили! Список результатов:', TEXT_WIDTH, contentCordX, contentCordY);
 
   var maxElement = getMaxElement(times);
+  var histogramCordX = cloudParams.X + (cloudParams.WIDTH - (barParams.WIDTH * times.length + barParams.GAP * (times.length - 1))) / 2;
 
   for (var i = 0; i < times.length; i++) {
-    var histogramCordX = CLOUD_X + (CLOUD_WIDTH - (BAR_WIDHT * times.length + BAR_SPACE_BETWEEN * (times.length - 1))) / 2;
     var barHeight = Math.round(times[i]) * barMaxHeight / maxElement;
-    var barCordX = histogramCordX + (BAR_WIDHT + BAR_SPACE_BETWEEN) * i;
-    var barCordY = contentCordY + LINE_HEIGHT * 2 + barMaxHeight - barHeight;
-    var playerName = names[i];
+    var barCordX = histogramCordX + (barParams.WIDTH + barParams.GAP) * i;
+    var barCordY = contentCordY + lineHeight * 2 + barMaxHeight - barHeight;
     var playerCordY = barCordY + barHeight + GAP;
-    var barColor = 'rgb(0, 0, ' + Math.round(Math.random() * 255) + ')';
+    var barColor = generateColor();
 
     if (names[i] === 'Вы') {
-      barColor = 'rgba(255, 0, 0, 1)';
+      barColor = barPlayerColor;
     }
 
-    buildingHistogram(ctx, barCordX, barCordY, BAR_WIDHT, barHeight, barColor, playerName, playerCordY);
+    buildHistogram(ctx, barCordX, barCordY, barParams.WIDTH, barHeight, barColor, names[i], playerCordY);
   }
 };
